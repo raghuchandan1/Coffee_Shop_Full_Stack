894cba470356a9b16969f10fb6ed5a6760f25d11
Raghu Chandan <33779280+Raghu-Chandan@users.noreply.github.com>
2020-07-26 23:47:04 +0530

    feat: Implemented errorhandler for AuthError

--- starter_code/backend/src/api.py
+++ starter_code/backend/src/api.py
@@ -199,15 +199,27 @@ def unprocessable(error):
 @DONE implement error handler for 404
     error handler should conform to general task above 
 '''
+
+
 @app.errorhandler(404)
 def not_found(error):
     return jsonify({
-                    "success": False,
-                    "error": 404,
-                    "message": "resource not found"
-                    }), 404
+        "success": False,
+        "error": 404,
+        "message": "resource not found"
+    }), 404
+
 
 '''
 @TODO implement error handler for AuthError
     error handler should conform to general task above 
 '''
+
+
+@app.errorhandler(AuthError)
+def auth_error(exception):
+    return jsonify({
+        "success": False,
+        "error": exception.status_code,
+        "message": exception.error
+    }), exception.status_code
